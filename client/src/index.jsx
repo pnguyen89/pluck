a248ecbd584bb1f6c54409b47682544b80fe188b
Frank D'Amico <frankadamicoaf@gmail.com>
2019-03-02 11:03:03 -0600

    (fix) Redirect to plantList on login

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -139,12 +139,13 @@ class App extends React.Component {
             <Switch>
               <Route path="/" render={() => <ZipCode onSubmit={this.zipCodeSubmit} />} exact />
               <Route path="/userProfile" render={() => <UserProfile plants={this.state.plants} onSubmit={this.submitUserInfo} />} />
-              <Route path="/plantList" render={() => <PlantList plants={this.state.plants} />} />
+              {/* <Route path="/plantList" render={() => <PlantList plants={this.state.plants} />} /> */}
               <Route path="/userLogin" render={() => <UserLogin plants={this.state.plants} zipcode={this.state.zipcode} onSubmit={this.userLogin} />} />
               <Route path="viewPlantProfile" render={() => <ViewPlantProfile userId={this.state.userId} />} />
               <Route path="/submitPlant" render={() => <CreatePlantProfile userId={this.state.userId} username={this.state.username} />} />
               <Route path="/myProfile" render={() => <MyProfile zipcode={this.state.zipcode} plants={this.state.userPlants} username={this.state.username} id={this.state.userId} handleChange={this.handleChange} />} />
               <Route path="/plantLocation" component={MapViewContainer} />
+              <Route path="/plantList" component={MapViewContainer} />
 
               <Route component={Error} />
             </Switch>
